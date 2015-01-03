b18ba1e4b27d91dfb835a55398d1c6e80214894c
chriselflord <elflord@hallbarteknik.com>
2015-01-03 17:24:07 -0500

    Update 750words.sh

--- 750words.sh
+++ 750words.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-touch /home/$(whoami)/750words/$(date +%F).md
-python3 /opt/extras.ubuntu.com/uberwriter/bin/uberwriter /home/$(whoami)/750words/$(date +%F).md
+mkdir /home/$(whoami)/YourDailyWrite/YourEntries
+touch /home/$(whoami)/YourDailyWrite/YourEntries/$(date +%F).md
+python3 /opt/extras.ubuntu.com/uberwriter/bin/uberwriter /home/$(whoami)/YourDailyWrite/YourEntries/$(date +%F).md
 
 # Requires zenity for popup notifications, not finished
 #MESSAGEHEADER="Number of words you have written today: "
 #zenity --info --text="$MESSAGEHEADER"
-#wc -w  < /home/$(whoami)/Documents/750words/$(date +%F).txt
+#wc -w  < /home/$(whoami)/YourDailyWrite/Entries/$(date +%F).txt
